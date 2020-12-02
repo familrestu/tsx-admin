ed4f6e3bc498c3d099da4f28c89610c5c487307f
Famil Restu Pambudi <famil.pambudi@gmail.com>
2020-12-02 21:43:13 +0700

    testing

--- src/screens/hris/attendance/AttendanceDataDetailsScreen.tsx
+++ src/screens/hris/attendance/AttendanceDataDetailsScreen.tsx
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
 import Page from 'components/Page';
+import Form from 'components/Form';
+import Input from 'components/Input';
 
 class AttendanceDataDetailsScreen extends Component<RouteComponentProps<{ id: string }>> {
     render() {
-        return <Page breadCrumb="Time & Attendance|Attendance Data|Details"></Page>;
+        return (
+            <Page breadCrumb="Attendance|Attendance Data|Details">
+                <Form>
+                    <Input type="text" label="Username" size="4" placeholder="username" name="username" row="false" />
+                    <Input type="select" label="Select Test" size="4" name="select_test" data="1=Male,2=Female" datasource="" />
+                </Form>
+            </Page>
+        );
     }
 }
 
--- src/screens/hris/attendance/AttendanceDataScreen.tsx
+++ src/screens/hris/attendance/AttendanceDataScreen.tsx
@@ -7,12 +7,13 @@ import Column from 'components/Column';
 class AttendanceDataScreen extends Component {
     render() {
         return (
-            <Page breadCrumb="Time & Attendance|Attendance Data">
+            <Page breadCrumb="Attendance|Attendance Data">
                 <Table datasource="attendance/AttData">
                     <Column label="Employee No" name="employee_no" type="link" link="/attendance/attendancedata/details/[date]" />
                     <Column label="Date" name="date" type="date" format="DD MMM, YYYY" width="350" />
-                    <Column label="Start Time" name="start_time" />
-                    <Column label="End Time" name="end_time" />
+                    <Column label="Start Time" name="start_time" type="time" />
+                    <Column label="End Time" name="end_time" type="time" />
+                    <Column label="Status" name="active" masking="0=Inactive,1=Active" />
                 </Table>
             </Page>
         );
