84efef4e92e69588cc2e9fcbb8f79a08198698ae
Famil Restu Pambudi <famil.pambudi@gmail.com>
2020-12-16 13:55:41 +0700

    adding datepicker type

--- src/screens/components/CalendarScreen.tsx
+++ src/screens/components/CalendarScreen.tsx
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 
 import Page from 'components/Page';
 import Calendar from 'components/Calendar';
+import { Card } from 'react-bootstrap';
 
 class CalendarScreen extends Component {
     render() {
         return (
             <Page breadCrumb="Components|Calendar">
-                <Calendar />
+                <Card className="shadow-sm card-border-left-success mb-4">
+                    <Card.Header className="calendar-toolbar" id="calendar-toolbar">
+                        <div className="left">Calendar</div>
+                        <div className="right" id="calendar-toolbar-right"></div>
+                    </Card.Header>
+                    <Card.Body>
+                        <Calendar ToolbarPosition="calendar-toolbar-right" />
+                    </Card.Body>
+                </Card>
+                <Card className="shadow-sm card-border-left-success">
+                    <Card.Header>Datepicker</Card.Header>
+                    <Card.Body>
+                        <Calendar type="datepicker" />
+                    </Card.Body>
+                </Card>
             </Page>
         );
     }
