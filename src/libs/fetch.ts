183781be60f20db5d70ee46c2856a927ac94c097
Famil Restu Pambudi <famil.pambudi@gmail.com>
2021-01-17 23:50:13 +0700

    Update fetch bugs

--- src/libs/fetch.ts
+++ src/libs/fetch.ts
@@ -1,35 +1,31 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 export const post = (data: any, link: string, config: AxiosRequestConfig, onSucess: (res: AxiosResponse) => void, onError: (err: AxiosError) => void) => {
-    if (process.env.REACT_APP_API_PATH) {
-        axios
-            .post(`${process.env.REACT_APP_API_PATH.replaceAll('/', '')}/${link}`, data, config)
-            .then((res: AxiosResponse) => {
-                if (onSucess) {
-                    onSucess(res);
-                }
-            })
-            .catch((err: AxiosError) => {
-                if (onError) {
-                    onError(err);
-                }
-            });
-    }
+    axios
+        .post(`${process.env.REACT_APP_API_PATH?.replaceAll('/', '')}/${link}`, data, config)
+        .then((res: AxiosResponse) => {
+            if (onSucess) {
+                onSucess(res);
+            }
+        })
+        .catch((err: AxiosError) => {
+            if (onError) {
+                onError(err);
+            }
+        });
 };
 
 export const get = (link: string, config: AxiosRequestConfig, onSucess: (res: AxiosResponse) => void, onError: (err: AxiosError) => void) => {
-    if (process.env.REACT_APP_API_PATH) {
-        axios
-            .get(`${process.env.REACT_APP_API_PATH.replaceAll('/', '')}/${link}`, config)
-            .then((res: AxiosResponse) => {
-                if (onSucess) {
-                    onSucess(res);
-                }
-            })
-            .catch((err: AxiosError) => {
-                if (onError) {
-                    onError(err);
-                }
-            });
-    }
+    axios
+        .get(`${process.env.REACT_APP_API_PATH?.replaceAll('/', '')}/${link}`, config)
+        .then((res: AxiosResponse) => {
+            if (onSucess) {
+                onSucess(res);
+            }
+        })
+        .catch((err: AxiosError) => {
+            if (onError) {
+                onError(err);
+            }
+        });
 };
