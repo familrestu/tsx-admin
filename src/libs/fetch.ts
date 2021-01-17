a67132762bb57603536662121e62c572d5af97f0
Famil Restu Pambudi <famil.pambudi@gmail.com>
2021-01-17 23:42:57 +0700

    Adjust fetch lib, to remove trailing slash

--- src/libs/fetch.ts
+++ src/libs/fetch.ts
@@ -1,31 +1,35 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 export const post = (data: any, link: string, config: AxiosRequestConfig, onSucess: (res: AxiosResponse) => void, onError: (err: AxiosError) => void) => {
-    axios
-        .post(`${process.env.REACT_APP_API_PATH}/${link}`, data, config)
-        .then((res: AxiosResponse) => {
-            if (onSucess) {
-                onSucess(res);
-            }
-        })
-        .catch((err: AxiosError) => {
-            if (onError) {
-                onError(err);
-            }
-        });
+    if (process.env.REACT_APP_API_PATH) {
+        axios
+            .post(`${process.env.REACT_APP_API_PATH.replaceAll('/', '')}/${link}`, data, config)
+            .then((res: AxiosResponse) => {
+                if (onSucess) {
+                    onSucess(res);
+                }
+            })
+            .catch((err: AxiosError) => {
+                if (onError) {
+                    onError(err);
+                }
+            });
+    }
 };
 
 export const get = (link: string, config: AxiosRequestConfig, onSucess: (res: AxiosResponse) => void, onError: (err: AxiosError) => void) => {
-    axios
-        .get(`${process.env.REACT_APP_API_PATH}/${link}`, config)
-        .then((res: AxiosResponse) => {
-            if (onSucess) {
-                onSucess(res);
-            }
-        })
-        .catch((err: AxiosError) => {
-            if (onError) {
-                onError(err);
-            }
-        });
+    if (process.env.REACT_APP_API_PATH) {
+        axios
+            .get(`${process.env.REACT_APP_API_PATH.replaceAll('/', '')}/${link}`, config)
+            .then((res: AxiosResponse) => {
+                if (onSucess) {
+                    onSucess(res);
+                }
+            })
+            .catch((err: AxiosError) => {
+                if (onError) {
+                    onError(err);
+                }
+            });
+    }
 };
