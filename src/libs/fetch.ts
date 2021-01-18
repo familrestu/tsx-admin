fc786253fad6112819989e32673c4bca0fa3e483
Famil Restu Pambudi <famil.pambudi@gmail.com>
2021-01-18 13:15:24 +0700

    fix path bugs

--- src/libs/fetch.ts
+++ src/libs/fetch.ts
@@ -1,8 +1,13 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 export const post = (data: any, link: string, config: AxiosRequestConfig, onSucess: (res: AxiosResponse) => void, onError: (err: AxiosError) => void) => {
+    let apiPath = '';
+    if (process.env.REACT_APP_API_PATH) {
+        apiPath = `${process.env.REACT_APP_API_PATH}/${link}`.replace(/([^:]\/)\/+/g, '$1');
+    }
+
     axios
-        .post(`${process.env.REACT_APP_API_PATH?.replaceAll('/', '')}/${link}`, data, config)
+        .post(apiPath, data, config)
         .then((res: AxiosResponse) => {
             if (onSucess) {
                 onSucess(res);
@@ -15,9 +20,14 @@ export const post = (data: any, link: string, config: AxiosRequestConfig, onSuce
         });
 };
 
-export const get = (link: string, config: AxiosRequestConfig, onSucess: (res: AxiosResponse) => void, onError: (err: AxiosError) => void) => {
+export const get = (link: string, config: AxiosRequestConfig, onSucess?: (res: AxiosResponse) => void, onError?: (err: AxiosError) => void) => {
+    let apiPath = '';
+    if (process.env.REACT_APP_API_PATH) {
+        apiPath = `${process.env.REACT_APP_API_PATH}/${link}`.replace(/([^:]\/)\/+/g, '$1');
+    }
+
     axios
-        .get(`${process.env.REACT_APP_API_PATH?.replaceAll('/', '')}/${link}`, config)
+        .get(apiPath, config)
         .then((res: AxiosResponse) => {
             if (onSucess) {
                 onSucess(res);
