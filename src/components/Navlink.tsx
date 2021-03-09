3568c43028c01e51a46f9f9fb6a5c6c54e499eb4
Famil Restu Pambudi <famil.pambudi@gmail.com>
2021-03-09 20:42:34 +0700

    Update link logic

--- src/components/Navlink.tsx
+++ src/components/Navlink.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { NavLink as NavLinkRRD, NavLinkProps } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { AppState } from 'redux/store';
 
 type NavlinkPropsType = {
     link?: string;
     navlink?: string;
+    accessmode?: AppState['PageState']['accessmode'];
 };
 
 const NavLink = (props: NavLinkProps & NavlinkPropsType) => {
     let url = props.navlink ? props.navlink : props.link ? props.link : props.to;
     const tempUrl: any = url;
     const dispatch = useDispatch();
-    const MenuAuthState = useSelector((state: AppState) => state.MenuAuthState);
 
     if (tempUrl.pathname !== undefined) {
         url = tempUrl.pathname;
@@ -22,12 +22,7 @@ const NavLink = (props: NavLinkProps & NavlinkPropsType) => {
         <NavLinkRRD
             {...props}
             onClick={(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-                const type = 'OPENPAGE';
-                const arrAuth = MenuAuthState.filter((a) => {
-                    return a.link === url;
-                });
-                const accessmode = arrAuth.length > 0 ? arrAuth[0].accessmode : 0;
-                dispatch({ type, path: url, accessmode });
+                dispatch({ type: 'OPENPAGE', path: url, accessmode: props.accessmode });
 
                 if (props.onClick) {
                     props.onClick(e);
