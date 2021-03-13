2a0f96c59210d07d9e03d211fb187df857f12eaa
Famil Restu Pambudi <famil.pambudi@gmail.com>
2021-03-13 22:51:37 +0700

    temporary remarked before delete

--- src/components/Navlink.tsx
+++ src/components/Navlink.tsx
@@ -1,35 +1,39 @@
 import React from 'react';
-import { NavLink as NavLinkRRD, NavLinkProps } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { AppState } from 'redux/store';
+// import { NavLink as NavLinkRRD, NavLinkProps } from 'react-router-dom';
+// import { useDispatch } from 'react-redux';
+// import { AppState } from 'redux/store';
 
-type NavlinkPropsType = {
-    link?: string;
-    navlink?: string;
-    accessmode?: AppState['PageState']['accessmode'];
-};
+// type NavlinkPropsType = {
+//     link?: string;
+//     navlink?: string;
+//     accessmode?: AppState['PageState']['accessmode'];
+// };
 
-const NavLink = (props: NavLinkProps & NavlinkPropsType) => {
-    let url = props.navlink ? props.navlink : props.link ? props.link : props.to;
-    const tempUrl: any = url;
-    const dispatch = useDispatch();
+// const NavLink = (props: NavLinkProps & NavlinkPropsType) => {
+//     let url = props.navlink ? props.navlink : props.link ? props.link : props.to;
+//     const tempUrl: any = url;
+//     const dispatch = useDispatch();
 
-    if (tempUrl.pathname !== undefined) {
-        url = tempUrl.pathname;
-    }
+//     if (tempUrl.pathname !== undefined) {
+//         url = tempUrl.pathname;
+//     }
 
-    return (
-        <NavLinkRRD
-            {...props}
-            onClick={(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-                dispatch({ type: 'OPENPAGE', path: url, accessmode: props.accessmode });
+//     return (
+//         <NavLinkRRD
+//             {...props}
+//             onClick={(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+//                 dispatch({ type: 'OPENPAGE', path: url, accessmode: props.accessmode });
 
-                if (props.onClick) {
-                    props.onClick(e);
-                }
-            }}
-        />
-    );
-};
+//                 if (props.onClick) {
+//                     props.onClick(e);
+//                 }
+//             }}
+//         />
+//     );
+// };
 
-export default NavLink;
+// export default NavLink;
+
+const Nothing = () => <></>;
+
+export default Nothing;
