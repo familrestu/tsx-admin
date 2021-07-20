d06f1160e48d12f308af30b07034a7ceb8f42482
Famil Restu Pambudi <famil.pambudi@gmail.com>
2021-07-20 19:31:11 +0700

    remove service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
@@ -26,4 +26,4 @@ ReactDOM.render(
     document.getElementById('root-container'),
 ); */
 
-serviceWorker.register();
+// serviceWorker.register();
